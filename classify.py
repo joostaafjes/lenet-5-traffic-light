178a3b0f90fc33a07c663fb32948038e48f1622f
joost aafjes <joostaafjes@gmail.com>
2019-06-21 09:57:20 +0200

    update classify

--- classify.py
+++ classify.py
@@ -7,7 +7,7 @@ class_model = load_model('model'+ '/model.h5')
 
 
 def predict(image_name):
-    img = load_img(image_name, False, target_size=(32, 32), color_mode='grayscale')
+    img = load_img(image_name, False, target_size=(32, 32), color_mode='rgb')
     x = img_to_array(img)
     x = np.expand_dims(x, axis=0)
     preds = class_model.predict_classes(x)
@@ -16,9 +16,9 @@ def predict(image_name):
 
 
 start = time.time()
-predict('images/0.jpeg')
+predict('images-resized/086-0-yellow.png.jpeg')
 elapsed = time.time() - start
 print('elapsed time:', elapsed, ' ms')
 
-predict('images/1.jpeg')
-predict('images/2.jpeg')
+predict('images-resized/067-2-green.png.jpeg')
+predict('images-resized/019-0-red.png.jpeg')
